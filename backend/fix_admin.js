60250fd3d02860d928b983e39c268e4513d2bd08
Keerakiat Kanchanawas <Keerakiat.K@ascggroup.localnet>
2026-08-27 15:28:18 +0700

    chore: remove sensitive env and one-off setup scripts from git tracking

--- backend/fix_admin.js
+++
@@ -1,4 +0,0 @@
-const db = require('./config/db');
-db.query('UPDATE employees SET email = "admin@company.com" WHERE id = 1')
-  .then(() => { console.log("Admin email reset"); process.exit(0); })
-  .catch(console.error);
